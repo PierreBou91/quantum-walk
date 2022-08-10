cd8ef88d0cbab0d1d7a85f56b1b49a03c8b22f6b
PierreBou <pierre.bouriat@outlook.fr>
2022-08-10 15:02:17 +0200

    feat: about and API page structure defined

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,7 +4,15 @@ type Props = {}
 
 const about = (props: Props) => {
 	return (
-		<div>about</div>
+		<div className={`container text-separators`}>
+			<h1>About</h1>
+			<h2>A word on parallel universes</h2>
+			<p>The QuantumWalk is Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ullam, ratione est sit veritatis eum ut enim, voluptatem libero similique error autem hic fugit et nulla expedita eius nisi aspernatur!</p>
+			<h2>What is the Quantum Walk ?</h2>
+			<p>The QuantumWalk is Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ullam, ratione est sit veritatis eum ut enim, voluptatem libero similique error autem hic fugit et nulla expedita eius nisi aspernatur!</p>
+			<h2>Main references</h2>
+			<p>The QuantumWalk is Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ullam, ratione est sit veritatis eum ut enim, voluptatem libero similique error autem hic fugit et nulla expedita eius nisi aspernatur!</p>
+		</div>
 	)
 }
 
--- pages/api-doc.tsx
+++ pages/api-doc.tsx
@@ -4,7 +4,18 @@ type Props = {}
 
 const apiDoc = (props: Props) => {
 	return (
-		<div>api documentations</div>
+		<div className='container text-separators'>
+			<h1>API</h1>
+			<h2>Reflexion</h2>
+			<p>If it becomes relevant I will expose an API to allow other developpers to query informations regarding the Quantum Walk Project</p>
+			<h2>Methods</h2>
+			<h3>1. GET distance</h3>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod nesciunt molestiae amet cumque eum repellat quas, non reiciendis maiores, modi asperiores nostrum quidem et autem, fuga illo magnam necessitatibus tenetur?</p>
+			<h3>2. GET past-steps</h3>
+			<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt reiciendis sequi at? Dolorem, eaque consectetur! Laborum velit quidem architecto nihil, aliquam autem, minus totam vel suscipit ad exercitationem tempore facilis!</p>
+			<h3>3. GET future-steps</h3>
+			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque corporis iure facere, eaque iusto pariatur dicta, placeat repellendus obcaecati non quam cupiditate ab sapiente in, eveniet cum quos ullam officiis?</p>
+		</div>
 	)
 }
 
